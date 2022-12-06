8908d43137c2c79ba2e0c08736dd37371bcd2153
JosuaMnl <josuamnalu@gmail.com>
2022-12-06 13:50:13 +0700

    Update

--- enkripsiString.py
+++ enkripsiString.py
@@ -7,16 +7,8 @@ text = input("Masukkan text yang ingin di enkripsi : ")
 dataEncrypt = {
     "text" : text
 }
-# print(json.dumps(dataEncrypt))
 # jsonData = urllib.request.urlopen(f"http://localhost/RemoteInvocation/enkripsiString.php?text={text}")
 jsonData = requests.post("http://localhost/RemoteInvocation/enkripsiString.php", data=dataEncrypt)
-print(type(jsonData.text))
-# print(jsonData.text)
-# # print(jsonData.text["text-encrypt"])
-# data = json.load(jsonData.json)
-data = dict(data=jsonData.text )
-data = data["data"]
-# print(data["text-encrypt"])
 
-# print(data)
-# print(data["text-encrypt"])
+data = json.loads(jsonData.text)
+print(data["text-encrypt"])
